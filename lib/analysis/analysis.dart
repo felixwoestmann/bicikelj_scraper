91bc8673126c2de7a282a175343c963cee3758fb
Felix Wöstmann <git@lostincoding.de>
2022-10-14 13:16:33 +0200

    update gitignroe

--- lib/analysis/analysis.dart
+++ lib/analysis/analysis.dart
@@ -30,7 +30,7 @@ void main() async {
 
 Future<List<List<BikeObservation>>> loadObservationsGroupedByBikeFromDB() async {
   final db = ObservationsDB();
-  await db.createConnectionToDB('/Users/felix/Desktop/bike_observations.db');
+  await db.createConnectionToDB('/Users/felix/Desktop/bicikle_scraper_output/20221014_bike_observations.db');
   final uniqueBikeNumbers = await db.getAllUniqueBikeNumbers();
   List<Future<List<BikeObservation>>> observationsGroupedByBikes =
       uniqueBikeNumbers.map((bikeNumber) => db.getAllObservationsForSingleBike(bikeNumber)).toList();
