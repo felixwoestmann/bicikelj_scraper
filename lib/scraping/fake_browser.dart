90aa954a010c322990ac2348174acc5f77f273f4
Felix Wöstmann <git@lostincoding.de>
2022-11-14 16:11:06 +0100

    Update fake_browser.dart

--- lib/scraping/fake_browser.dart
+++ lib/scraping/fake_browser.dart
@@ -3,7 +3,10 @@ import 'package:puppeteer/puppeteer.dart';
 class FakeBrowser {
   static Future<String> obtainRefreshToken(String refreshTokenKey) async {
     // Download the Chromium binaries, launch it and connect to the "DevTools"
-    var browser = await puppeteer.launch();
+    var browser = await puppeteer.launch(
+      headless: true,
+      args: ['--no-sandbox'],
+    );
     var page = await browser.newPage();
     // Go to a page and wait to be fully loaded, then get LocalStorage
     await page.goto('https://www.bicikelj.si/en/mapping', wait: Until.networkIdle);
